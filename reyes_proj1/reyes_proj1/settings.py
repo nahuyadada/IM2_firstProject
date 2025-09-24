bf91779eeff0602d0c7446e64acfbda1ccedb7e3
LAB13_USER ON WS32 <L13X16W32@LabHCI.net>
2025-09-24 16:58:36 +0800

    feature(reyes_app):setup_templates

--- reyes_proj1/reyes_proj1/settings.py
+++ reyes_proj1/reyes_proj1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'reyes_proj1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [reyes_app],
+        'DIRS': ['templates/pos_app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
